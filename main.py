f8b0628c78cbdea24f196939647727a0a1f6a1fd
Azman0101 <azman0101@gmail.com>
2025-10-25 13:15:07 +0200

    chore: disable video

--- main.py
+++ main.py
@@ -56,7 +56,6 @@ async def start_agent_session(user_id: str):
     # Setup RunConfig
     run_config = RunConfig(
         streaming_mode="bidi",
-        session_resumption=types.SessionResumptionConfig(transparent=False),
         realtime_input_config=types.RealtimeInputConfig(
             automatic_activity_detection=types.AutomaticActivityDetection(
                 start_of_speech_sensitivity=types.StartSensitivity.START_SENSITIVITY_LOW,
